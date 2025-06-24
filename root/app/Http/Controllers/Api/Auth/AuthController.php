74fb5d7dac19a9a29ac34b0c8a8bb9d74b4b5a0e
doannvph33201 <nguyendoan7370@gmail.com>
2025-06-24 17:09:19 +0700

    feat(auth): auth

---
+++ root/app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|max:255|min:1',
+        ]);
+
+        $user = User::create([
+            'user_id' => \Str::uuid(),
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => \Hash::make($data['password']),
+        ]);
+
+        return $this->responseSuccess([
+            [
+                'name' => $user['name'],
+                'email' => $user['email'],
+            ],
+            Response::HTTP_CREATED,
+            'Đăng ký thành công'
+        ]);
+    }
+
+    public function login()
+    {
+        $data = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($data)) {
+            return $this->responseSuccess(
+                null,
+                Response::HTTP_UNAUTHORIZED,
+                'Thông tin đăng nhập không đúng'
+            );
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function verifyToken()
+    {
+        try {
+            $user = auth()->user();
+            if (!$user) {
+
+                return $this->responseSuccess(
+                    [
+                        'data' => false
+                    ],
+                    Response::HTTP_UNAUTHORIZED,
+                    'Error'
+                );
+            }
+
+            return $this->responseSuccess(
+                [
+                    'data' => true
+                ],
+                Response::HTTP_OK,
+                'Keng'
+            );
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Token lỗi'], 401);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Đăng xuất thành công']);
+    }
+
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
